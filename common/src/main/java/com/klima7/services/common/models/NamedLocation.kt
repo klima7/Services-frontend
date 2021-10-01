885a9671a2c66bd4643528af2470f22b5e897ed2
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-01 17:43:58 +0200

    Domain models associated with experts created

---
+++ common/src/main/java/com/klima7/services/common/models/NamedLocation.kt
@@ -0,0 +1,6 @@
+package com.klima7.services.common.models
+
+data class NamedLocation(
+    val name: String,
+    val coords: Coordinates
+)
---
+++ common/src/main/java/com/klima7/services/common/models/WorkingArea.kt
@@ -0,0 +1,6 @@
+package com.klima7.services.common.models
+
+data class WorkingArea(
+    val location: NamedLocation,
+    val radius: Int
+)
