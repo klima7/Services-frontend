5a91b418ee55d2316df3299a89e4d2b4a555b17b
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-05 01:52:11 +0200

    Home bottom navigation added

--- expert/src/main/java/com/klima7/services/expert/features/home/HomeFragment.kt
+++ expert/src/main/java/com/klima7/services/expert/features/home/HomeFragment.kt
@@ -1,5 +1,9 @@
 package com.klima7.services.expert.features.home
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.klima7.services.common.lib.base.BaseFragment
 import com.klima7.services.expert.R
 import com.klima7.services.expert.databinding.FragmentHomeBinding
@@ -12,4 +16,9 @@ class HomeFragment: BaseFragment<FragmentHomeBinding>() {
     override val layoutId = R.layout.fragment_home
     override val viewModel: HomeViewModel by viewModel()
 
+    override fun init() {
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.homeBottomNav.setupWithNavController(navController)
+    }
 }
