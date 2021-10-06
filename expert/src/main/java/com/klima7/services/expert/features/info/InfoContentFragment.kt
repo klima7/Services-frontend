1496e18cd9e49ee1c855f0f17a1c471fb9859ed5
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-07 01:43:03 +0200

    Info fields appearance improved

--- expert/src/main/java/com/klima7/services/expert/features/info/InfoContentFragment.kt
+++ expert/src/main/java/com/klima7/services/expert/features/info/InfoContentFragment.kt
@@ -1,12 +1,15 @@
 package com.klima7.services.expert.features.info
 
+import androidx.lifecycle.lifecycleScope
 import com.klima7.services.common.lib.failurable.FailurableFragment
 import com.klima7.services.expert.R
-import com.klima7.services.expert.databinding.FragmentLoginBinding
+import com.klima7.services.expert.databinding.FragmentInfoBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class InfoContentFragment: FailurableFragment<FragmentLoginBinding>() {
+class InfoContentFragment: FailurableFragment<FragmentInfoBinding>() {
 
     override val layoutId = R.layout.fragment_info
     override val viewModel: InfoContentViewModel by viewModel()
@@ -14,5 +17,13 @@ class InfoContentFragment: FailurableFragment<FragmentLoginBinding>() {
     override fun onFirstCreation() {
         super.onFirstCreation()
         viewModel.doSomething()
+    }
+
+    init {
+        lifecycleScope.launch {
+            delay(2000)
+            binding.infoName.error = "To pole jest wymagane"
+            binding.infoPhone.error = "To pole jest wymagane"
+        }
     }
 }
