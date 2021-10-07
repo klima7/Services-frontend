b5bb5dd04739d78a48c770aba738fdc4cf2088b8
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-08 01:03:16 +0200

    Restoring Avatar state added

--- common/src/main/java/com/klima7/services/common/lib/avatar/Avatar.kt
+++ common/src/main/java/com/klima7/services/common/lib/avatar/Avatar.kt
@@ -1,6 +1,7 @@
 package com.klima7.services.common.lib.avatar
 
 import android.content.Context
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.FrameLayout
@@ -10,12 +11,19 @@ import com.bumptech.glide.signature.ObjectKey
 import com.klima7.services.common.R
 import com.klima7.services.common.data.di.EMULATE
 import de.hdodenhof.circleimageview.CircleImageView
+import android.os.Bundle
+
+
+
 
 class Avatar(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
     private var uri: String? = null
+    private var signature: Long? = null
 
     init {
+        Log.i("Avatarr","Creating avatar")
+
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.Avatar,
@@ -31,7 +39,22 @@ class Avatar(context: Context, attrs: AttributeSet?) : FrameLayout(context, attr
         }
     }
 
+    override fun onSaveInstanceState(): Parcelable? {
+        val bundle = Bundle()
+        bundle.putLong("signature", signature!!)
+        bundle.putParcelable("superState", super.onSaveInstanceState())
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val bundle = state as Bundle
+        signature = bundle.getLong("signature")
+        super.onRestoreInstanceState(bundle.getParcelable("superState"))
+        refreshView()
+    }
+
     fun setUri(uri: String) {
+        this.signature = System.currentTimeMillis()
         this.uri = uri
         refreshView()
         invalidate()
@@ -41,11 +64,14 @@ class Avatar(context: Context, attrs: AttributeSet?) : FrameLayout(context, attr
     private fun refreshView() {
         val fixedUri = if(EMULATE) uri?.replace("localhost", "10.0.2.2") else uri
         val image = findViewById<CircleImageView>(R.id.avatar_image)
-        Log.i("Hello", "Avatar! Setting image to '$fixedUri'")
         Glide.with(context).clear(image)
+
+        if(signature == null)
+            signature = System.currentTimeMillis()
+
         Glide.with(this)
             .load(fixedUri)
-            .signature(ObjectKey(System.currentTimeMillis()))
+            .signature(ObjectKey(signature!!))
             .placeholder(R.drawable.image_profile_default)
             .error(R.drawable.image_profile_default)
             .into(image)
