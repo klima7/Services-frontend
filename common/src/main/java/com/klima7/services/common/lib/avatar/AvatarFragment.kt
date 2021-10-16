7d1cc9436b4942746560c5778a09a07b2e7a5884
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-16 17:04:11 +0200

    Animations added to Avatar

--- common/src/main/java/com/klima7/services/common/lib/avatar/AvatarFragment.kt
+++ common/src/main/java/com/klima7/services/common/lib/avatar/AvatarFragment.kt
@@ -1,12 +1,17 @@
 package com.klima7.services.common.lib.avatar
 
+import android.graphics.Bitmap
 import android.util.Log
+import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.signature.ObjectKey
 import com.klima7.services.common.R
 import com.klima7.services.common.data.di.EMULATE
 import com.klima7.services.common.databinding.FragmentAvatarBinding
 import com.klima7.services.common.domain.models.ProfileImage
+import com.klima7.services.common.lib.GlideApp
 import com.klima7.services.common.lib.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -34,10 +39,9 @@ class AvatarFragment : BaseFragment<FragmentAvatarBinding>() {
 
         Glide.with(this).clear(image)
 
-        Log.i("Glide", "Signature is ${profileImage.changeTime}")
-
         Glide.with(this)
             .load(fixedUri)
+            .transition(withCrossFade(resources.getInteger(R.integer.avatar_fade_time)))
             .signature(ObjectKey(profileImage.changeTime))
             .placeholder(R.drawable.image_profile_default)
             .error(R.drawable.image_profile_default)
