608d723ea00bb8fd398eac56c2e8aeb24d72e5bc
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-16 18:36:59 +0200

    AvatarView fixed

--- common/src/main/java/com/klima7/services/common/lib/avatar/AvatarView.kt
+++ common/src/main/java/com/klima7/services/common/lib/avatar/AvatarView.kt
@@ -4,66 +4,69 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.AttributeSet
-import android.util.Log
 import android.widget.FrameLayout
+import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.signature.ObjectKey
 import com.klima7.services.common.R
 import com.klima7.services.common.data.di.EMULATE
+import com.klima7.services.common.domain.models.ProfileImage
 
 class AvatarView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
-    private var uri: String = ""
-    private var signature: Long = System.currentTimeMillis()
+    private var profileImage: ProfileImage? = null
 
     init {
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.Avatar,
-            0, 0).apply {
-
-            try {
-                uri = getText(R.styleable.Avatar_uri)?.toString() ?: ""
-            } finally {
-                inflate(context, R.layout.view_avatar, this@AvatarView)
-                recycle()
-
-                refreshView()
-            }
-        }
+        inflate(context, R.layout.view_avatar, this)
+        refreshView()
     }
 
-    override fun onSaveInstanceState(): Parcelable? {
+    override fun onSaveInstanceState(): Parcelable {
         val bundle = Bundle()
-        bundle.putLong("signature", signature)
+        val isNull = profileImage == null
+        bundle.putBoolean("isNull", isNull)
+        if(!isNull) {
+            bundle.putLong("changeTime", profileImage?.changeTime ?: 0L)
+            bundle.putString("url", profileImage?.url ?: "")
+        }
         bundle.putParcelable("superState", super.onSaveInstanceState())
         return bundle
     }
 
     override fun onRestoreInstanceState(state: Parcelable?) {
         val bundle = state as Bundle
-        signature = bundle.getLong("signature")
+        val isNull = bundle.getBoolean("isNull")
+        if(!isNull) {
+            val url = bundle.getString("url", "")
+            val changeTime = bundle.getLong("changeTime")
+            profileImage = ProfileImage(url, changeTime)
+        }
         super.onRestoreInstanceState(bundle.getParcelable("superState"))
         refreshView()
     }
 
-    fun setUri(uri: String) {
-        this.signature = System.currentTimeMillis()
-        this.uri = uri
+    fun setProfileImage(profileImage: ProfileImage?) {
+        this.profileImage = profileImage
         refreshView()
         invalidate()
         requestLayout()
     }
 
     private fun refreshView() {
-//        val fixedUri = "https://firebasestorage.googleapis.com/v0/b/services-a7389.appspot.com/o/profile_images%2FpnmE94NiTQbRV3tuUWYf0Pj3XHq1.png?alt=media&token=e5b9e28d-0a84-4f70-ae62-60a005ec564e"
-//        Log.i("Glide", "Loading: $fixedUri")
-//        val image = findViewById<CircleImageView>(R.id.avatar_image)
-//        Glide.with(context).clear(image)
-//        Glide.with(this)
-//            .load(fixedUri)
-//            .placeholder(R.drawable.image_profile_default)
-//            .error(R.drawable.image_profile_default)
-//            .into(image)
+        val constProfileImage = profileImage ?: return
+
+        val fixedUri = if(EMULATE) constProfileImage.url.replace("localhost", "10.0.2.2") else constProfileImage.url
+        val image = findViewById<ImageView>(R.id.avatarview_image)
+
+        Glide.with(this).clear(image)
+
+        Glide.with(this)
+            .load(fixedUri)
+            .transition(DrawableTransitionOptions.withCrossFade(resources.getInteger(R.integer.avatar_fade_time)))
+            .signature(ObjectKey(constProfileImage.changeTime))
+            .placeholder(R.drawable.avatar_placeholder)
+            .error(R.drawable.avatar_failure)
+            .into(image)
     }
 }
