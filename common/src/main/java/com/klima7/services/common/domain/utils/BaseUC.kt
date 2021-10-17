9203e46173df29e8044fec1f623039b59a9b948f
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-17 17:45:35 +0200

    BaseUC class created

---
+++ common/src/main/java/com/klima7/services/common/domain/utils/BaseUC.kt
@@ -0,0 +1,21 @@
+package com.klima7.services.common.domain.utils
+
+import com.klima7.services.common.domain.models.Failure
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class BaseUC<P, R> {
+
+    abstract suspend fun execute(params: P): Outcome<Failure, R>
+
+    fun start(scope: CoroutineScope, params: P, onFailure: (Failure)->Unit, onSuccess: (R)->Unit) {
+        scope.launch {
+            execute(params).foldS({ failure ->
+                onFailure(failure)
+            }, { result ->
+                onSuccess(result)
+            })
+        }
+    }
+
+}
