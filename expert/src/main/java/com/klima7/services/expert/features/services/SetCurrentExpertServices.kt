d3b8a4fa66fad03d1ce6fe8454ee650e634a5861
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-17 22:37:32 +0200

    setCurrentExpertServicesUC extracted

---
+++ expert/src/main/java/com/klima7/services/expert/features/services/SetCurrentExpertServices.kt
@@ -0,0 +1,19 @@
+package com.klima7.services.expert.features.services
+
+import com.klima7.services.common.data.repositories.ExpertsRepository
+import com.klima7.services.common.domain.models.Failure
+import com.klima7.services.common.domain.models.Service
+import com.klima7.services.common.domain.utils.BaseUC
+import com.klima7.services.common.domain.utils.Outcome
+
+class SetCurrentExpertServices(
+    private val expertsRepository: ExpertsRepository
+): BaseUC<SetCurrentExpertServices.Params, BaseUC.NoResult>() {
+
+    override suspend fun execute(params: Params): Outcome<Failure, NoResult> {
+        val servicesIds = params.services.map { it.id }
+        return expertsRepository.setServicesIds(servicesIds)
+    }
+
+    data class Params(val services: List<Service>)
+}
