d6b8cb1707182b37033c87705d2aaea7ebc216d7
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-21 21:06:25 +0200

    CommentFragment renamed to RatingFragment etc

--- common/src/main/java/com/klima7/services/common/ui/comment/CommentViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.klima7.services.common.ui.comment
-
-import com.klima7.services.common.ui.base.BaseViewModel
-
-class CommentViewModel: BaseViewModel() {
-}
---
+++ common/src/main/java/com/klima7/services/common/ui/rating/RatingViewModel.kt
@@ -0,0 +1,6 @@
+package com.klima7.services.common.ui.rating
+
+import com.klima7.services.common.ui.base.BaseViewModel
+
+class RatingViewModel: BaseViewModel() {
+}
