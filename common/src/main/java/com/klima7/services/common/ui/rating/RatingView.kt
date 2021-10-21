00b3f3e73e5c579f5645e956ef80c6fff59bf796
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-22 01:27:18 +0200

    Appearance of comments improved

--- common/src/main/java/com/klima7/services/common/ui/rating/RatingView.kt
+++ common/src/main/java/com/klima7/services/common/ui/rating/RatingView.kt
@@ -7,6 +7,10 @@ import com.klima7.services.common.R
 import com.klima7.services.common.databinding.ViewRatingBinding
 import android.view.LayoutInflater
 import androidx.databinding.DataBindingUtil
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
 
 
 class RatingView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
@@ -31,11 +35,13 @@ class RatingView(context: Context, attrs: AttributeSet?) : FrameLayout(context, 
     private fun refreshView() {
         val cRwpi = rwpi ?: return
         val cRating = cRwpi.rating
-        val cProfileImage = cRwpi.profileImage
 
         binding.ratingComment.text = cRating.comment
         binding.ratingServiceName.text = cRating.serviceName
         binding.ratingRating.rating = cRating.rating.toFloat()
-        binding.ratingAvatar.setProfileImage(cProfileImage)
+        binding.ratingClientName.text = cRating.clientName
+
+        val format = SimpleDateFormat("dd.MM.yyy", Locale.US)
+        binding.ratingDate.text = format.format(cRating.date)
     }
 }
