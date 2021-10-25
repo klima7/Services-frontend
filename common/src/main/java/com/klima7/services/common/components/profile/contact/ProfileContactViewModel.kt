a27f133502d53d3c3b657c4a17cf1f4ded089f68
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-25 08:52:47 +0200

    Phone on profile screen formatted

--- common/src/main/java/com/klima7/services/common/components/profile/contact/ProfileContactViewModel.kt
+++ common/src/main/java/com/klima7/services/common/components/profile/contact/ProfileContactViewModel.kt
@@ -14,12 +14,18 @@ class ProfileContactViewModel: BaseViewModel() {
     val websiteVisible = info.map { it.website != null }
     val noContactVisible = info.map { it.phone == null && it.email == null && it.website == null }
 
-    val phoneText = info.map { it.phone }
+    val phoneText = info.map { formatPhone(it.phone) }
     val emailText = info.map { it.email }
     val websiteText = info.map { it.website }
 
     fun setInfo(info: ExpertInfo) {
         this.info.value = info
+    }
+
+    private fun formatPhone(phone: String?): String? {
+        if (phone == null || phone.length != 9)
+            return phone
+        return "${phone.substring(0, 3)} ${phone.substring(3, 6)} ${phone.substring(6, 9)}"
     }
 
 }
