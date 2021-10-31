a6924f4274fed3d2c7bda25a09aee355c3288326
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-31 08:59:42 +0100

    Toolbar added to job view

--- expert/src/main/java/com/klima7/services/expert/features/job/JobFragment.kt
+++ expert/src/main/java/com/klima7/services/expert/features/job/JobFragment.kt
@@ -1,13 +1,11 @@
 package com.klima7.services.expert.features.job
 
-import android.util.Log
-import com.klima7.services.common.platform.BaseLoadFragment
+import com.klima7.services.common.platform.BaseFragment
 import com.klima7.services.expert.R
 import com.klima7.services.expert.databinding.FragmentJobBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.lang.Error
 
-class JobFragment: BaseLoadFragment<FragmentJobBinding>() {
+class JobFragment: BaseFragment<FragmentJobBinding>() {
 
     override val layoutId = R.layout.fragment_job
     override val viewModel: JobViewModel by viewModel()
@@ -16,11 +14,6 @@ class JobFragment: BaseLoadFragment<FragmentJobBinding>() {
 
     override fun init() {
         super.init()
-
-        val args = arguments
-        Log.i("Hello", "Arguments: $args")
-        Log.i("Hello", "Arguments: ${args?.getString("jobId")}")
-
 
         viewModel.job.observe(viewLifecycleOwner, { job ->
             if(job != null) {
@@ -33,7 +26,6 @@ class JobFragment: BaseLoadFragment<FragmentJobBinding>() {
         super.onFirstCreation()
 
         jobId = arguments?.getString("jobId") ?: throw Error("JobId argument not supplied")
-        Log.i("Hello", "JobId=$jobId")
         viewModel.start(jobId)
     }
 }
--- expert/src/main/java/com/klima7/services/expert/features/job/JobViewModel.kt
+++ expert/src/main/java/com/klima7/services/expert/features/job/JobViewModel.kt
@@ -2,58 +2,47 @@ package com.klima7.services.expert.features.job
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.klima7.services.common.components.views.LoadAreaView
 import com.klima7.services.common.models.Failure
 import com.klima7.services.common.models.Job
-import com.klima7.services.common.platform.BaseLoadViewModel
+import com.klima7.services.common.platform.BaseViewModel
 import com.klima7.services.common.usecases.GetJobUC
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class JobViewModel(
     private val getJobUC: GetJobUC
-): BaseLoadViewModel() {
+): BaseViewModel() {
 
     private lateinit var jobId: String
     val job = MutableLiveData<Job>()
 
     val loadState = MutableLiveData(LoadAreaView.State.LOAD)
-    val failure = MutableLiveData<Failure>()
+    val loadFailure = MutableLiveData<Failure>()
+    val title = job.map { job -> job.serviceName }
+    val subtitle = job.map { job -> job.clientName }
 
     fun start(jobId: String) {
         this.jobId = jobId
         loadContent()
-
-        viewModelScope.launch {
-            delay(3000)
-            failure.value = Failure.InternetFailure
-            loadState.value = LoadAreaView.State.FAILURE
-            delay(3000)
-            loadState.value = LoadAreaView.State.MAIN
-            delay(3000)
-            loadState.value = LoadAreaView.State.PENDING
-            delay(3000)
-            loadState.value = LoadAreaView.State.MAIN
-        }
     }
 
-    override fun refresh() {
-        Log.i("Hello", "Refreshing in VM")
+    fun refresh() {
         loadContent()
     }
 
     private fun loadContent() {
-        showLoading()
+        loadState.value = LoadAreaView.State.LOAD
         getJobUC.start(
             viewModelScope,
             GetJobUC.Params(jobId),
             { failure ->
-                showFailure(failure)
+                loadFailure.value = failure
+                loadState.value = LoadAreaView.State.FAILURE
             },
             { job ->
                 this.job.value = job
-                showMain()
+                loadState.value = LoadAreaView.State.MAIN
             }
         )
     }
