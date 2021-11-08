97b79bf7a83faa34023446ff6bf83c8840f334f2
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-08 17:57:21 +0100

    Client profile toolbar subtitle and back button

--- client/src/main/java/com/klima7/services/client/features/profile/ProfileFragment.kt
+++ client/src/main/java/com/klima7/services/client/features/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.klima7.services.client.features.profile
 
+import androidx.lifecycle.map
 import com.klima7.services.client.R
 import com.klima7.services.client.databinding.FragmentProfileBinding
 import com.klima7.services.common.platform.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.exp
 
 class ProfileFragment: BaseFragment<FragmentProfileBinding>() {
 
@@ -16,5 +18,17 @@ class ProfileFragment: BaseFragment<FragmentProfileBinding>() {
         val expertUid = arguments?.getString("expertUid") ?: throw Error("expertUid argument not supplied")
         childFragmentManager.findFragmentById(R.id.profile_fragment)
             ?.getViewModel<ProfileContentViewModel>()?.start(expertUid)
+    }
+
+    override fun init() {
+        super.init()
+
+        binding.profileToolbar.setNavigationOnClickListener { requireActivity().onBackPressed() }
+
+        childFragmentManager.findFragmentById(R.id.profile_fragment)
+            ?.getViewModel<ProfileContentViewModel>()?.expert?.map { expert -> expert.info.name }
+            ?.observe(viewLifecycleOwner, { expertName ->
+                binding.profileToolbar.subtitle = expertName
+            })
     }
 }
