bd9c9600b8f0c9c90df5e1e8040ec4d55154d900
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-19 22:25:21 +0100

    Gray messages on the right

--- common/src/main/java/com/klima7/services/common/components/msgviewer/ImageMessageItem.kt
+++ common/src/main/java/com/klima7/services/common/components/msgviewer/ImageMessageItem.kt
@@ -1,5 +1,8 @@
 package com.klima7.services.common.components.msgviewer
 
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.TypedValue
 import android.view.Gravity
 import com.bumptech.glide.Glide
 import com.klima7.services.common.R
@@ -20,9 +23,12 @@ class ImageMessageItem(
             if (side == Side.LEFT) {
                 msgimageContainer.gravity = Gravity.LEFT
                 msgimageContainer.setPadding(0, 0, convertDpToPixel(64f, msgimageContainer.context), 0)
+                binding.cardColor = binding.msgimageCard.context.resources.getColor(R.color.left_message_cloud_color, null)
+
             } else if (side == Side.RIGHT) {
                 msgimageContainer.gravity = Gravity.RIGHT
                 msgimageContainer.setPadding(convertDpToPixel(64f, msgimageContainer.context), 0, 0, 0)
+                binding.cardColor = getRightColor(binding.msgimageCard.context)
             }
         }
 
@@ -35,6 +41,14 @@ class ImageMessageItem(
             .placeholder(R.drawable.avatar_placeholder)
             .error(R.drawable.avatar_failure)
             .into(image)
+    }
+
+    private fun getRightColor(context: Context): Int {
+        val typedValue = TypedValue()
+        val a: TypedArray = context.obtainStyledAttributes(typedValue.data, intArrayOf(R.attr.messageCloudColor))
+        val color = a.getColor(0, 0)
+        a.recycle()
+        return color
     }
 
     override fun getLayout(): Int {
--- common/src/main/java/com/klima7/services/common/components/msgviewer/TextMessageItem.kt
+++ common/src/main/java/com/klima7/services/common/components/msgviewer/TextMessageItem.kt
@@ -1,8 +1,10 @@
 package com.klima7.services.common.components.msgviewer
 
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
-import androidx.core.view.marginLeft
 import com.klima7.services.common.R
 import com.klima7.services.common.databinding.ElementTextMessageBinding
 import com.klima7.services.common.models.TextMessage
@@ -23,12 +25,22 @@ class TextMessageItem(
             if(side == Side.LEFT) {
                 msgtextContainer.gravity = Gravity.LEFT
                 msgtextContainer.setPadding(0, 0, convertDpToPixel(64f, msgtextContainer.context), 0)
+                binding.cardColor = binding.msgtextCard.context.resources.getColor(R.color.left_message_cloud_color, null)
             }
             else if(side == Side.RIGHT) {
                 msgtextContainer.gravity = Gravity.RIGHT
                 msgtextContainer.setPadding(convertDpToPixel(64f, msgtextContainer.context), 0, 0, 0)
+                binding.cardColor = getRightColor(binding.msgtextCard.context)
             }
         }
+    }
+
+    private fun getRightColor(context: Context): Int {
+        val typedValue = TypedValue()
+        val a: TypedArray = context.obtainStyledAttributes(typedValue.data, intArrayOf(R.attr.messageCloudColor))
+        val color = a.getColor(0, 0)
+        a.recycle()
+        return color
     }
 
     override fun getLayout(): Int {
