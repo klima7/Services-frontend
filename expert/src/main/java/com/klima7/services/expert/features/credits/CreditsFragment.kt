840657be430f57560e0f25ccd8926c16716792a7
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-21 18:35:46 +0100

    Offers views appearance improved

--- expert/src/main/java/com/klima7/services/expert/features/credits/CreditsFragment.kt
+++ expert/src/main/java/com/klima7/services/expert/features/credits/CreditsFragment.kt
@@ -2,8 +2,11 @@ package com.klima7.services.expert.features.credits
 
 import com.klima7.services.common.components.credits.BaseCreditsFragment
 import com.klima7.services.common.components.credits.Credit
+import com.klima7.services.expert.R
 
 class CreditsFragment: BaseCreditsFragment() {
 
-    override fun getSpecificCredits() = listOf<Credit>()
+    override fun getSpecificCredits() = listOf(
+        Credit(R.drawable.deal_hand, R.string.credit_deal_hand)
+    )
 }
