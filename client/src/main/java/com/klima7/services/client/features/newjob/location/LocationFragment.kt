44bdbb0acab38880afe876ff4537cb8e87f9342e
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-24 11:00:53 +0100

    LocationPickerItem removed

--- client/src/main/java/com/klima7/services/client/features/newjob/location/LocationFragment.kt
+++ client/src/main/java/com/klima7/services/client/features/newjob/location/LocationFragment.kt
@@ -1,6 +1,12 @@
 package com.klima7.services.client.features.newjob.location
 
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.klima7.services.client.R
 import com.klima7.services.client.databinding.FragmentLocationBinding
 import com.klima7.services.client.databinding.FragmentServiceBinding
@@ -20,6 +26,7 @@ class LocationFragment: BaseFragment<FragmentLocationBinding>() {
 
     override fun onFirstCreation() {
         super.onFirstCreation()
+        configureAutocomplete()
         val serviceId = arguments?.getString("serviceId") ?: throw Error("serviceId argument not supplied")
         val serviceName = arguments?.getString("serviceName") ?: throw Error("serviceName argument not supplied")
         viewModel.start(serviceId, serviceName)
@@ -35,7 +42,18 @@ class LocationFragment: BaseFragment<FragmentLocationBinding>() {
             layoutManager = LinearLayoutManager(requireContext())
         }
 
-        groupieAdapter += ProgressItem(2, "Wybierz lokalizację")
+//        groupieAdapter += ProgressItem(2, "Wybierz lokalizację")
+    }
+
+    private fun configureAutocomplete() {
+        val autocompleteFragment =
+            childFragmentManager.findFragmentById(R.id.location_autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
+        autocompleteFragment.setTypeFilter(TypeFilter.CITIES)
+        autocompleteFragment.setCountries("pl")
+
     }
 
 }
