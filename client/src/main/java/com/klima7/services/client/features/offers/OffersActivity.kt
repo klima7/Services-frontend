1c015b3c55c113466efa904ad137eb6a10f495e6
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-26 01:28:28 +0100

    OffersActivity animations

--- client/src/main/java/com/klima7/services/client/features/offers/OffersActivity.kt
+++ client/src/main/java/com/klima7/services/client/features/offers/OffersActivity.kt
@@ -1,7 +1,18 @@
 package com.klima7.services.client.features.offers
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.klima7.services.common.platform.BaseActivity
 
 class OffersActivity: BaseActivity() {
     override fun fragment() = OffersFragment()
+
+    override fun overrideStartTransition() {
+        super.overrideStartTransition()
+        Animatoo.animateSlideRight(this)
+    }
+
+    override fun overrideExitTransition() {
+        super.overrideExitTransition()
+        Animatoo.animateSlideLeft(this)
+    }
 }
