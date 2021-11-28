9ed81ec09dc832895d7e2696b86924209196f97d
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-29 00:51:45 +0100

    ContactTileView created and applied

---
+++ common/src/main/java/com/klima7/services/common/components/profile/contact/ContactTileView.kt
@@ -0,0 +1,57 @@
+package com.klima7.services.common.components.profile.contact
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.databinding.BindingMethod
+import androidx.databinding.DataBindingUtil
+import com.klima7.services.common.R
+import com.klima7.services.common.components.views.JobView
+import com.klima7.services.common.databinding.ViewContactTileBinding
+
+@androidx.databinding.BindingMethods(
+    value = [
+        BindingMethod(
+            type = ContactTileView::class,
+            attribute = "contactTile_subtitle",
+            method = "setSubtitle"
+        ),
+    ]
+)
+class ContactTileViewBindingMethods
+
+class ContactTileView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+    private var binding: ViewContactTileBinding
+    private var title: String? = null
+    private var subtitle: String? = null
+    private var icon: Drawable? = null
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.view_contact_tile, this, true)
+        attrs?.let { initTypedArray(it) }
+        refreshView()
+    }
+
+    private fun initTypedArray(attrs: AttributeSet) {
+        val ta = context.theme.obtainStyledAttributes(attrs, R.styleable.ContactTile, 0, 0)
+        title = ta.getString(R.styleable.ContactTile_contactTile_title)
+        subtitle = ta.getString(R.styleable.ContactTile_contactTile_subtitle)
+        icon = ta.getDrawable(R.styleable.ContactTile_contactTile_icon)
+        ta.recycle()
+    }
+
+    fun setSubtitle(subtitle: String?) {
+        this.subtitle = subtitle
+        refreshView()
+    }
+
+    private fun refreshView() {
+        binding.title = title
+        binding.subtitle = subtitle
+        binding.icon = icon
+    }
+}
