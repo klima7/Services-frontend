fd02c182a2c382ae8093c951afec55f28c79c12e
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-29 00:15:19 +0100

    Better default profile image

--- common/src/main/java/com/klima7/services/common/components/views/AvatarView.kt
+++ common/src/main/java/com/klima7/services/common/components/views/AvatarView.kt
@@ -71,8 +71,9 @@ class AvatarView(context: Context, attrs: AttributeSet?) : FrameLayout(context, 
         if(constProfileImage == null) {
             Glide.with(this).clear(image)
             Glide.with(this)
-                .load(R.drawable.avatar_default2)
-                .transition(DrawableTransitionOptions.withCrossFade(resources.getInteger(R.integer.avatar_fade_time)))
+                .load(R.drawable.avatar_default)
+                .transition(DrawableTransitionOptions.withCrossFade(
+                    resources.getInteger(R.integer.avatar_fade_time)))
                 .into(image)
             return
         }
