32f7b199284e0212eab3409e86b579e23bb13bc6
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-12-18 12:06:37 +0100

    Updating token every launch removed

--- client/src/main/java/com/klima7/services/client/features/splash/SplashViewModel.kt
+++ client/src/main/java/com/klima7/services/client/features/splash/SplashViewModel.kt
@@ -1,22 +1,7 @@
 package com.klima7.services.client.features.splash
 
-import androidx.lifecycle.viewModelScope
-import com.klima7.services.client.messaging.UpdateTokenUC
 import com.klima7.services.common.components.splash.BaseSplashViewModel
-import com.klima7.services.common.core.None
 
 class SplashViewModel(
     getCurrentClientStateUC: GetCurrentClientStateUC,
-): BaseSplashViewModel(getCurrentClientStateUC) {
-
-    override fun started(offerId: String?) {
-        super.started(offerId)
-        UpdateTokenUC().start(
-            viewModelScope,
-            None(),
-            {},
-            {}
-        )
-    }
-
-}
+): BaseSplashViewModel(getCurrentClientStateUC)
--- expert/src/main/java/com/klima7/services/expert/features/splash/SplashViewModel.kt
+++ expert/src/main/java/com/klima7/services/expert/features/splash/SplashViewModel.kt
@@ -1,22 +1,7 @@
 package com.klima7.services.expert.features.splash
 
-import androidx.lifecycle.viewModelScope
 import com.klima7.services.common.components.splash.BaseSplashViewModel
-import com.klima7.services.common.core.None
-import com.klima7.services.expert.messaging.UpdateTokenUC
 
 class SplashViewModel(
     getCurrentExpertStateUC: GetCurrentExpertStateUC
-): BaseSplashViewModel(getCurrentExpertStateUC) {
-
-    override fun started(offerId: String?) {
-        super.started(offerId)
-        UpdateTokenUC().start(
-            viewModelScope,
-            None(),
-            {},
-            {}
-        )
-    }
-
-}
+): BaseSplashViewModel(getCurrentExpertStateUC)
